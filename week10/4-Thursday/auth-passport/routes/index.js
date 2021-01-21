8bf2b2a41886b43ec1b605a9970be8b77045b8fa
Kanny <kanny.ghafour87@gmail.com>
2021-01-21 13:55:39 -0600

    pull

--- week10/4-Thursday/auth-passport/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-
-router("/", (req,res) => {
-    res.send("homepage");
-    
-})
-
-module.exports =router;
--- week10/4-Thursday/auth-passport/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-
-router("/login", (req,res) => {
-    res.send("homepage");
-    
-})
-
-module.exports =router;
